0a6102b302ad8e8620453d21dcceafb450cf8439
german nunez <german90nunez@gmail.com>
2021-07-08 22:57:07 -0500

    new resume link

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
         <p>Kevin Garcia</p>
         <div>Currently under maintenice. please download resume <span style={{"font-style": "normal", "font-size": "2.5rem"}}>👷</span></div>
         <button className="download__resume">
-          <a href="https://drive.google.com/file/d/1hF6cbRrarDyQOFiAAkIixWvTr9KlA-Be/view?usp=sharing">Download Resume</a>
+          <a href="https://drive.google.com/file/d/1DJMhNXjavjXuJfTpfrKObC60INm2zOnU/view?usp=sharing">Download Resume</a>
         </button> 
       </div>
     </div>
