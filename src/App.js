b56799a11c9f66ce1354aa6b78a5dcde09504547
german nunez <german90nunez@gmail.com>
2021-07-08 20:46:58 -0500

    added a little more to coming soon

--- src/App.js
+++ src/App.js
@@ -4,8 +4,11 @@ function App() {
   return (
     <div className="App">
       <div className="coming__soon">
-        <h1>Kevin Garcia</h1>
-        <h5>Coming soon</h5>
+        <p>Kevin Garcia</p>
+        <div>Currently under maintenice. please download resume <span style={{"font-style": "normal", "font-size": "2.5rem"}}>👷</span></div>
+        <button className="download__resume">
+          <a href="https://drive.google.com/file/d/1hF6cbRrarDyQOFiAAkIixWvTr9KlA-Be/view?usp=sharing" target="_blank">Download Resume</a>
+        </button> 
       </div>
     </div>
   );
